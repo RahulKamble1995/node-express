2042252b791fd9f5f17e676072ee23a83fcfe1c4
Rahul9.Kamble <rahul9.kamble@ril.com>
2022-11-21 19:48:12 +0530

    Assignment 1

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const port = 3000;
 const app = express();
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
 
 app.use((req, res, next)=>{
     //console.log(req.headers);
